a6a8e376e29c713e348b5f0142cce7c0791fef2b
Дмитрий Сергеев <kolovrat-86@yandex.ru>
2022-09-30 18:45:02 +0300

    Файл

--- Валюты/MainWindow.xaml.cs
+++ Валюты/MainWindow.xaml.cs
@@ -12,9 +12,26 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Валюты
 {
+    struct Money
+    {
+        public string rub;
+        public string kop;
+        public string usd;
+        public string eur;
+        public string nir;
+        public string isk;
+        public string cny;
+
+        public override string ToString()
+        {
+            return rub + ";" + kop + ";" + usd + ";" + eur + ";" + nir + ";" + isk + ";" + cny;
+        }
+    }
+
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -86,23 +103,43 @@ namespace Валюты
 
                 try
                 {
+                    if (Convert.ToInt32(tbRUBinp.Text) < 0)
+                    {
+                        spHomeOut.Visibility = Visibility.Collapsed;
+                        MessageBox.Show("Количество рублей не может быть меньше 0", "Ввод значений", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
+
+                    if (Convert.ToInt32(tbKOPinp.Text) < 0 || Convert.ToInt32(tbKOPinp.Text) > 99)
+                    {
+                        spHomeOut.Visibility = Visibility.Collapsed;
+                        MessageBox.Show("Введите количество копеек от 0 до 99", "Ввод значений", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
+
+                    if (Convert.ToInt32(tbRUBinp.Text) == 0 && Convert.ToInt32(tbKOPinp.Text) == 0)
+                    {
+                        spHomeOut.Visibility = Visibility.Collapsed;
+                        MessageBox.Show("Количество рублей и копеек не может быть равно 0", "Ввод значений", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
 
                     switch (tbHomeOut.Text)
                     {
                         case "Доллары: ":
-                            res = (Convert.ToDouble(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.017;
+                            res = (Convert.ToInt32(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.017;
                             break;
                         case "Евро: ":
-                            res = (Convert.ToDouble(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.018;
-                            break;
-                        case "Тенге: ":
-                            res = (Convert.ToDouble(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 8.33;
-                            break;
-                        case "Гривны: ":
-                            res = (Convert.ToDouble(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.63;
-                            break;
-                        case "Лиры: ":
-                            res = (Convert.ToDouble(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.32;
+                            res = (Convert.ToInt32(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.018;
+                            break;                                       
+                        case "Тенге: ":                                 
+                            res = (Convert.ToInt32(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 8.33;
+                            break;                                        
+                        case "Гривны: ":                                  
+                            res = (Convert.ToInt32(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.63;
+                            break;                                       
+                        case "Лиры: ":                                    
+                            res = (Convert.ToInt32(tbRUBinp.Text) + Convert.ToDouble(tbKOPinp.Text) / 100) * 0.32;
                             break;
                         default:
                             MessageBox.Show("Выберите валюту, в которую необходимо конвертировать рубли", "Выбор валюты", MessageBoxButton.OK, MessageBoxImage.Information);
@@ -126,6 +163,13 @@ namespace Валюты
                 tbKopOut.Text = "Копейки: ";
                 try
                 {
+                    if (Convert.ToDouble(tbForeignVal.Text) <= 0)
+                    {
+                        spHomeOut.Visibility = Visibility.Collapsed;
+                        MessageBox.Show("Количество валюты не может быть меньше 0", "Ввод значений", MessageBoxButton.OK, MessageBoxImage.Information);
+                        return;
+                    }
+
                     switch (tbForeignInp.Text)
                     {
 
@@ -134,14 +178,14 @@ namespace Валюты
                             break;
                         case "Евро: ":
                             res = Convert.ToDouble(tbForeignVal.Text) * 56.18;
-                            break;
-                        case "Тенге: ":
+                            break;          
+                        case "Тенге: ":     
                             res = Convert.ToDouble(tbForeignVal.Text) * 0.12;
-                            break;
-                        case "Гривны: ":
+                            break;         
+                        case "Гривны: ":   
                             res = Convert.ToDouble(tbForeignVal.Text) * 1.58;
-                            break;
-                        case "Лиры: ":
+                            break;          
+                        case "Лиры: ":      
                             res = Convert.ToDouble(tbForeignVal.Text) * 3.15;
                             break;
                         default:
@@ -231,5 +275,111 @@ namespace Валюты
         {
             spChoiceHomeland.Visibility = Visibility.Visible;
         }
+
+        private void btnFile_Click(object sender, RoutedEventArgs e)
+        {
+            List<Money> list = new List<Money>();
+            getData("dataMoney.csv", list);
+
+        }
+
+        static List<Money> createList(List<Money> list)
+        {
+            List<Money> newList = new List<Money>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                string rub = list[i].rub;
+                string kop = list[i].kop;
+                string usd = list[i].usd;
+                string eur = list[i].eur;
+                string nir = list[i].nir;
+                string isk = list[i].isk;
+                string cny = list[i].cny;
+
+                //if (rub || !int.TryParse(rub, out int result))
+                //{
+                //    rub = null;
+                //}
+
+                //newList.Add(new Money
+                //{
+
+                //});
+            }
+
+            return newList;
+        }
+
+        static void getData(string path, List<Money> list)
+        {
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] array = sr.ReadLine().Split(';');
+
+                    string rub = array[0];
+                    string kop = array[1];
+                    string usd = array[2];
+                    string eur = array[3];
+                    string nir = array[4];
+                    string isk = array[5];
+                    string cny = array[6];
+
+                    if (rub == "" || !int.TryParse(rub, out int result))
+                    {
+                        rub = null;
+                    }
+                    if (kop == "" || !int.TryParse(kop, out result))
+                    {
+                        kop = null;
+                    }
+                    if (usd == "" || !int.TryParse(usd, out result))
+                    {
+                        usd = null;
+                    }
+                    if (eur == "" || !int.TryParse(eur, out result))
+                    {
+                        eur = null;
+                    }
+                    if (nir == "" || !int.TryParse(nir, out result))
+                    {
+                        nir = null;
+                    }
+                    if (isk == "" || !int.TryParse(isk, out result))
+                    {
+                        isk = null;
+                    }
+                    if (cny == "" || !int.TryParse(cny, out result))
+                    {
+                        cny = null;
+                    }
+
+                    list.Add(new Money()
+                    {
+                        rub = rub,
+                        kop = kop,
+                        usd = usd,
+                        eur = eur,
+                        nir = nir,
+                        isk = isk,
+                        cny = cny
+                    });
+                }
+            }
+        }
+
+        static void inputData(string path, List<Money> list)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                foreach (Money item in list)
+                {
+                    sw.WriteLine(item.ToString());
+                }
+            }
+        }
+
     }
 }
